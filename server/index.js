073bb863792313d2204cd28bc637d693d7bf96a2
Patrick <patthenoble0@gmail.com>
2021-07-23 17:11:38 -0400

    Fixed server, cleaned index.html code, played with portfolio styles

--- server/index.js
+++ server/index.js
@@ -1,17 +1,11 @@
 import express from 'express';
-import path from 'path';
 import cors from 'cors';
-/*import cookieParser from 'cookie-parser';
-import logger from 'morgan';*/
-/*import indexRouter from './routes/index';
-import usersRouter from './routes/users';*/
 const PORT = process.env.PORT || 5000;
-
 import { Octokit } from "@octokit/rest";
-const octokit = new Octokit({auth: process.env.GITHUB_REST_API_KEY});
+const octokit = new Octokit({ auth: process.env.GITHUB_REST_API_KEY }); // Using Heroku environment variable to protect API key.
 
 var corsOptions = {
-  origin: 'https://patthenoble.github.io',
+  origin: ['https://patthenoble.github.io', 'http://localhost:3000'],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
@@ -19,52 +13,35 @@ var corsOptions = {
 
 
 var app = express();
-//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-/*app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));*/
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-app.get('/', cors(corsOptions), (req, res) => res.json({"thing": "new thing 2"}));
-app.get('/github-repos', cors(corsOptions), async(req, res) => {
-  try{
-    res.json({"data": await octokit.request('GET /user/repos')})
+app.get('/', cors(corsOptions), (req, res) => res.json({ "defaultJson": "default" }));
+app.get('/github-data', cors(corsOptions), async (req, res) => {
+  const repos = await octokit.request('GET /user/repos', {
+    accept: "application/vnd.github.v3.json",
+    mediaType: { // You must provide a custom media type in the Accept header like this in order to receive topics from the github rest api.
+      previews: [
+        'mercy'
+      ]
+    }
+  });
+  var readMes = [];
+  for (var i = 0; i < repos.data.length; i++) {
+    const currentName = repos.data[i].name;
+    try {
+      const currentReadMe = await octokit.request('GET /repos/{owner}/{repo}/readme', {
+        accept: "application/vnd.github.v3.raw",
+        owner: 'PatTheNoble',
+        repo: currentName
+      });
+      readMes.push(currentReadMe);
+    } 
+    catch (error) {
+      console.log(error);
+    }
   }
-  catch(err){
-    console.log(err)
-  }
+  const data = {"repos": repos, "readMes": readMes};
+  res.json({ "data": data });
 })
-app.get('/octokit', cors(corsOptions), (req, res) => res.json({"octokit": octokit}));
-
-/*app.use('/', indexRouter);
-app.use('/users', usersRouter);*/
 export default app;
-
-
-
-
-  /*
-  const app = express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  */
-
-
-  /*.get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
-  app.get('/github-repos', (req, res) => {
-    res.json({"data": "data"})
-  })*/
-
-
-  /*
-  app.get('/github-repos', async(req, res) => {
-    try{
-      res.json({"data": await octokit.request('GET /user/repos')})
-    }
-    catch(err){
-      console.log(err)
-    }
-  })
-  */
