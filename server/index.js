25e62a93a42e640b3475252d06d83cc4cc42efb3
Patrick <patthenoble0@gmail.com>
2021-07-23 08:43:03 -0400

    Get octokit from backend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 /*import cookieParser from 'cookie-parser';
 import logger from 'morgan';*/
 /*import indexRouter from './routes/index';
@@ -8,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 import { Octokit } from "@octokit/rest";
 const octokit = new Octokit({auth: process.env.GITHUB_REST_API_KEY});
+
+var corsOptions = {
+  origin: 'https://patthenoble.github.io',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 
 
@@ -19,8 +25,8 @@ app.use(express.urlencoded({ extended: false }));
 /*app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));*/
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-app.get('/', (req, res) => res.json({"thing": "thing"}));
-app.get('/github-repos', async(req, res) => {
+app.get('/', cors(corsOptions), (req, res) => res.json({"thing": "new thing 2"}));
+app.get('/github-repos', cors(corsOptions), async(req, res) => {
   try{
     res.json({"data": await octokit.request('GET /user/repos')})
   }
@@ -28,6 +34,7 @@ app.get('/github-repos', async(req, res) => {
     console.log(err)
   }
 })
+app.get('/octokit', cors(corsOptions), (req, res) => res.json({"octokit": octokit}));
 
 /*app.use('/', indexRouter);
 app.use('/users', usersRouter);*/
