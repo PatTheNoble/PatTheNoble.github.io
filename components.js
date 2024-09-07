5b5d2ed4b3e2cede0818e3ad8afeb91c4eeeb149
PatTheNoble <patthenoble0@gmail.com>
2024-09-07 15:53:04 -0400

    Updated Unit manager page with gifs. Added Favicon.

--- components.js
+++ components.js
@@ -1,8 +1,16 @@
-function loadComponent(componentId, componentFile) {
+function loadComponent(componentId, componentFile, callback = null) {
     fetch(componentFile)
         .then(response => response.text())
         .then(data => {
-            document.getElementById(componentId).innerHTML = data;
+            const element = document.getElementById(componentId);
+            const originalContent = element.innerHTML; // Save existing content
+            element.innerHTML = data; // Load component
+            if (originalContent) {
+                element.querySelector('.banner-text').innerHTML += originalContent; // Insert original content into banner-text
+            }
+
+            // Execute callback if provided (for additional customization)
+            if (callback) callback(element);
         })
         .catch(error => console.error('Error loading component:', error));
 }
